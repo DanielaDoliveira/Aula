02121a752b20d327d087400e6790e42211b7cc97
Daniela <danidaninpce3@gmail.com>
2024-10-02 20:59:27 -0300

    criar moedas

---
+++ CriarMoedas.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CriarMoedas : MonoBehaviour
+{
+  
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Moedas()
+    {
+    
+      
+      
+    }
+}
